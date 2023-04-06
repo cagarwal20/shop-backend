fc722c3cb3500989863fb210a382ae122df869fa
cagarwal20 <cagarwal20@gmail.com>
2023-04-06 14:36:03 +0530

    made changes to the backend for sort

--- products/views.py
+++ products/views.py
@@ -22,19 +22,19 @@ def get_products(request):
         filters&=Q(Q(description__icontains=search) | Q(name__icontains=search))
     if "sort_by" in params:
         sort_by = params.get('sort_by')
-        if sort_by==1: #price low to high
+        if sort_by=="1": #price low to high
             objects = Products.objects.filter(filters).values('name','description','ratings','image','mrp','disc','sale_price').order_by('sale_price')
-        if sort_by==2: #price high to low
+        if sort_by=="2": #price high to low
             objects = Products.objects.filter(filters).values('name','description','ratings','image','mrp','disc','sale_price').order_by('-sale_price')
-        if sort_by==3: #discount low to high
+        if sort_by=="3": #discount low to high
             objects = Products.objects.filter(filters).values('name','description','ratings','image','mrp','disc','sale_price').order_by('disc')
-        if sort_by==4:
+        if sort_by=="4":
             objects = Products.objects.filter(filters).values('name','description','ratings','image','mrp','disc','sale_price').order_by('-disc')
-    
-    objects = Products.objects.filter(filters).values('name','description','ratings','image','mrp','disc','sale_price')
-    # if len(search)==0:
-    #     objects = Products.objects.all().values('name','description','ratings','image','mrp','disc','sale_price')
-    #     # ser_data = ProductSerializer(objects,many=True)
-    #     # print(ser_data.data)
-    # objects = Products.objects.filter(Q(description__icontains=search) | Q(name__icontains=search)).values('name','description','ratings','image','mrp','disc','sale_price')
-    return Response({"data":objects},status=200)
+        return Response({"data":objects},status=200)
+    # objects = Products.objects.filter(filters).values('name','description','ratings','image','mrp','disc','sale_price')
+    # # if len(search)==0:
+    # #     objects = Products.objects.all().values('name','description','ratings','image','mrp','disc','sale_price')
+    # #     # ser_data = ProductSerializer(objects,many=True)
+    # #     # print(ser_data.data)
+    # # objects = Products.objects.filter(Q(description__icontains=search) | Q(name__icontains=search)).values('name','description','ratings','image','mrp','disc','sale_price')
+    # return Response({"data":objects},status=200)
